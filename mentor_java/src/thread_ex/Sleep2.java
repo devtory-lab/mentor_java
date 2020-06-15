e6f4ec932be8b2e1e61f6b1bafa0915950c5694a
an44ajh <an44ajh@gmail.com>
2020-06-15 16:20:08 +0900

    thread sleep

---
+++ mentor_java/src/thread_ex/Sleep2.java
@@ -0,0 +1,33 @@
+package thread_ex;
+
+class SleepThread extends Thread{
+	
+	public void run() {
+		
+		System.out.println("카운트다운 5초");
+		for (int i = 5; i >= 0; i--) {
+			System.out.println(i);
+			if (i != 0) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ie) {
+					System.out.println(ie.toString());
+				}
+			}
+		}
+		System.out.println("종료");
+	}
+	
+	
+}
+
+public class Sleep2 {
+
+	public static void main(String[] args) {
+		
+		SleepThread t = new SleepThread();
+		t.start();
+
+	}
+
+}
