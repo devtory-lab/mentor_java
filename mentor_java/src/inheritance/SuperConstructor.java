758712be1559215ea8bc5a920cef4d6466a1cb03
an44ajh <an44ajh@gmail.com>
2020-06-15 11:04:47 +0900

    상속 super()

---
+++ mentor_java/src/inheritance/SuperConstructor.java
@@ -0,0 +1,33 @@
+package inheritance;
+
+class Car2{
+	int wheel;
+	int speed;
+	String color;
+	
+	Car2(String color) {
+		this.color = color;
+	}
+}
+
+class SportsCar extends Car2{
+	int speedLimit;
+
+	SportsCar(String color, int speedLimit) {
+		super(color);
+		this.speedLimit = speedLimit;
+	}
+	
+}
+
+public class SuperConstructor {
+
+	public static void main(String[] args) {
+		
+		SportsCar s1 = new SportsCar("red", 330);
+		System.out.println(s1.color);
+		System.out.println(s1.speedLimit);
+
+	}
+
+}
