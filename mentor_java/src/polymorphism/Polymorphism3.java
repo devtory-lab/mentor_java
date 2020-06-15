e1c1be98ab04cdaa5159ae92f6c6c6cb5884bf8b
an44ajh <an44ajh@gmail.com>
2020-06-15 11:28:52 +0900

    다형성

---
+++ mentor_java/src/polymorphism/Polymorphism3.java
@@ -0,0 +1,32 @@
+package polymorphism;
+
+class Animal{
+	void breath(){
+		System.out.println("숨쉬기");
+	}
+}
+
+class Lion extends Animal{
+
+	public String toString() {
+		return "사자";
+	}
+	
+}
+
+class ZooKeeper{
+	void feed(Lion lion) {
+		System.out.println(lion + "에게 고기 주기");
+	}
+}
+
+public class Polymorphism3 {
+
+	public static void main(String[] args) {
+		Lion lion1 = new Lion();
+		ZooKeeper james = new ZooKeeper();		
+		james.feed(lion1);
+
+	}
+
+}
