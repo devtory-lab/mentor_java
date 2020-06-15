34156e9a00be42f7b6526331aec091ddd65f6aff
devtory <66317486+devtory-lab@users.noreply.github.com>
2020-06-15 10:32:21 +0900

    Update HelloWorld.java

--- mentor_java/src/sample/HelloWorld.java
+++ mentor_java/src/sample/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World~~");
 		System.out.println("2222222222222");
 		System.out.println("33333333333333");
+		System.out.println("444444444444444");
 
 	}
 
